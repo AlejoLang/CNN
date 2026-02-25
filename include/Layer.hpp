5f0014deace01bf7ac707d590b84f4d2364e5fc5
Alejo Lang <alejolang123@gmail.com>
2026-02-24 21:59:39 -0300

    Fix virtual methods definitions

--- include/Layer.hpp
+++ include/Layer.hpp
@@ -1,9 +1,14 @@
 #pragma once
+#include <Activations.hpp>
 #include <Tensor3.hpp>
 
 class Layer {
+protected:
+  ActivationFunction activation;
+
 public:
-  Layer();
-  virtual Tensor3<float> forward(Tensor3<float> input);
-  virtual Tensor3<float> backwards(Tensor3<float> deltas);
+  Layer(ActivationFunction activation = ActivationFunction::NONE) : activation(activation) {};
+  virtual Tensor3<float> forward(Tensor3<float> input) = 0;
+  virtual Tensor3<float> backwards(Tensor3<float> deltas) = 0;
+  virtual ~Layer() = default;
 };
