9707b3365937317284474d46b7891d7210733018
Alejo Lang <alejolang123@gmail.com>
2026-02-25 15:12:03 -0300

    Implement backwards method for convolutional layer

--- include/ConvolutionalLayer.hpp
+++ include/ConvolutionalLayer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <Activations.hpp>
 #include <Layer.hpp>
+#include <Matrix.hpp>
 #include <vector>
 
 class ConvolutionalLayer : public Layer {
@@ -10,10 +11,14 @@ private:
   int filterDepth;
   std::vector<Tensor3<float>> filters;
   Tensor3<float> biases;
+  Matrix<float> flatLastInput;
+  Matrix<float> flatActivations;
+  Matrix<float> deltas;
 
 public:
   ConvolutionalLayer(int filterSize, int filterDepth, int filterCount,
                      ActivationFunction activation = RELU);
   Tensor3<float> forward(Tensor3<float> input) override;
-  Tensor3<float> backwards(Tensor3<float> deltas) override;
+  Tensor3<float> backwards(Tensor3<float> prevLayerDeltas) override;
+  void update(float learningRate) override;
 };
--- src/ConvolutionalLayer.cpp
+++ src/ConvolutionalLayer.cpp
@@ -20,6 +20,7 @@ ConvolutionalLayer::ConvolutionalLayer(int filterSize, int filterDepth, int filt
 
 Tensor3<float> ConvolutionalLayer::forward(Tensor3<float> input) {
   Matrix<float> flatInput = im2col<float>(input, this->filterSize, this->filterDepth);
+  this->flatLastInput = flatInput;
   Matrix<float> flatFilters =
       Matrix<float>(this->filterCount, this->filterSize * this->filterSize * this->filterDepth);
   for (size_t f = 0; f < filterCount; f++) {
@@ -35,6 +36,7 @@ Tensor3<float> ConvolutionalLayer::forward(Tensor3<float> input) {
     }
   }
   Matrix<float> featureMat = cross(flatInput, flatFilters);
+  this->flatActivations = Matrix<float>(featureMat.getNumCols(), featureMat.getNumRows());
   int slidesW = input.getWidth() - this->filterSize + 1;
   int slidesH = input.getHeight() - this->filterSize + 1;
   Tensor3<float> featureTens = Tensor3<float>(slidesW, slidesH, this->filterCount);
@@ -43,6 +45,7 @@ Tensor3<float> ConvolutionalLayer::forward(Tensor3<float> input) {
     int col = y % slidesW;
     for (size_t x = 0; x < featureMat.getNumCols(); x++) {
       float value = featureMat.getValue(x, y) + this->biases.getValue(0, 0, x);
+      this->flatActivations.setValue(x, y, value);
       if (this->activation == RELU) {
         value = relu(value);
       } else if (this->activation == SIGMOID) {
@@ -54,4 +57,81 @@ Tensor3<float> ConvolutionalLayer::forward(Tensor3<float> input) {
   return featureTens;
 }
 
-Tensor3<float> ConvolutionalLayer::backwards(Tensor3<float> deltas) {}
+Tensor3<float> ConvolutionalLayer::backwards(Tensor3<float> prevLayerDeltas) {
+  Matrix<float> flatDeltas = im2col<float>(prevLayerDeltas, 1, this->filterCount);
+  this->deltas =
+      hadamard(flatDeltas, apply(this->flatActivations,
+                                 this->activation == RELU ? reluDerivative : sigmoidDerivative));
+  Matrix<float> flatFilters =
+      Matrix<float>(this->filterCount, this->filterSize * this->filterSize * this->filterDepth);
+  for (size_t f = 0; f < filterCount; f++) {
+    for (size_t c = 0; c < filterDepth; c++) {
+      int channel = c * this->filterSize * this->filterSize;
+      for (size_t y = 0; y < filterSize; y++) {
+        int row = y * this->filterSize;
+        for (size_t x = 0; x < filterSize; x++) {
+          float val = this->filters[f].getValue(x, y, c);
+          flatFilters.setValue(f, channel + row + x, val);
+        }
+      }
+    }
+  }
+  Matrix<float> prevDeltas = cross(this->deltas, transpose(flatFilters));
+  int inputW = prevLayerDeltas.getWidth() + this->filterSize - 1;
+  int inputH = prevLayerDeltas.getHeight() + this->filterSize - 1;
+  Tensor3<float> result = Tensor3<float>(inputW, inputH, this->filterDepth);
+  for (size_t col = 0; col < prevDeltas.getNumRows(); col++) {
+    int outY = col / inputW;
+    int outX = col % inputW;
+    for (size_t c = 0; c < (size_t)this->filterDepth; c++) {
+      int channelBase = c * this->filterSize * this->filterSize;
+      for (size_t fy = 0; fy < (size_t)this->filterSize; fy++) {
+        for (size_t fx = 0; fx < (size_t)this->filterSize; fx++) {
+          int inY = outY - (int)fy;
+          int inX = outX - (int)fx;
+          if (inY >= 0 && inX >= 0 && inY < inputH && inX < inputW) {
+            float v = result.getValue(inX, inY, c);
+            v += prevDeltas.getValue(channelBase + fy * this->filterSize + fx, col);
+            result.setValue(inX, inY, c, v);
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+
+void ConvolutionalLayer::update(float learningRate) {
+  Matrix<float> flatFilters =
+      Matrix<float>(this->filterCount, this->filterSize * this->filterSize * this->filterDepth);
+  for (size_t f = 0; f < filterCount; f++) {
+    for (size_t c = 0; c < filterDepth; c++) {
+      int channel = c * this->filterSize * this->filterSize;
+      for (size_t y = 0; y < filterSize; y++) {
+        int row = y * this->filterSize;
+        for (size_t x = 0; x < filterSize; x++) {
+          float val = this->filters[f].getValue(x, y, c);
+          flatFilters.setValue(f, channel + row + x, val);
+        }
+      }
+    }
+  }
+  Matrix<float> weightDeltas = cross(transpose(this->flatLastInput), this->deltas);
+  for (size_t f = 0; f < filterCount; f++) {
+    for (size_t c = 0; c < filterDepth; c++) {
+      int channel = c * this->filterSize * this->filterSize;
+      for (size_t y = 0; y < filterSize; y++) {
+        int row = y * this->filterSize;
+        for (size_t x = 0; x < filterSize; x++) {
+          float val = flatFilters.getValue(f, channel + row + x) -
+                      learningRate * weightDeltas.getValue(f, channel + row + x);
+          this->filters[f].setValue(x, y, c, val);
+        }
+      }
+    }
+  }
+  for (size_t f = 0; f < filterCount; f++) {
+    float biasVal = this->biases.getValue(0, 0, f) - learningRate * this->deltas.getValue(f, 0);
+    this->biases.setValue(0, 0, f, biasVal);
+  }
+}
