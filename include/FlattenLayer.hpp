9f35ae1e05afab5042441e05c8a59e5714b40a14
Alejo Lang <alejolang123@gmail.com>
2026-02-25 15:11:31 -0300

    Implement backwards method for flatten layer

--- include/FlattenLayer.hpp
+++ include/FlattenLayer.hpp
@@ -9,5 +9,5 @@ private:
 public:
   FlattenLayer(int inputWidth, int inputHeight, int inputDepth);
   Tensor3<float> forward(Tensor3<float> input) override;
-  Tensor3<float> backwards(Tensor3<float> deltas) override;
+  Tensor3<float> backwards(Tensor3<float> prevLayerDeltas) override;
 };
--- src/FlattenLayer.cpp
+++ src/FlattenLayer.cpp
@@ -22,4 +22,17 @@ Tensor3<float> FlattenLayer::forward(Tensor3<float> input) {
   return output;
 }
 
-Tensor3<float> FlattenLayer::backwards(Tensor3<float> deltas) {}
+Tensor3<float> FlattenLayer::backwards(Tensor3<float> prevLayerDeltas) {
+  Tensor3<float> output = Tensor3<float>(this->inputWidth, this->inputHeight, this->inputDepth);
+  for (size_t c = 0; c < output.getChannels(); c++) {
+    int channelBase = c * output.getWidth() * output.getHeight();
+    for (size_t y = 0; y < output.getHeight(); y++) {
+      int rowBase = channelBase + (y * output.getWidth());
+      for (size_t x = 0; x < output.getWidth(); x++) {
+        float val = prevLayerDeltas.getValue(rowBase + x, 0, 0);
+        output.setValue(x, y, c, val);
+      }
+    }
+  }
+  return output;
+}
