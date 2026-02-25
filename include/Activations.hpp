7671d3a4a359c55ad801f18201ddc2923f437e9f
Alejo Lang <alejolang123@gmail.com>
2026-02-24 21:59:57 -0300

    Set the activaiton functions as inline

--- include/Activations.hpp
+++ include/Activations.hpp
@@ -3,16 +3,16 @@
 
 enum ActivationFunction { RELU, SIGMOID, NONE };
 
-float relu(float x) {
+inline float relu(float x) {
   return x > 0 ? x : 0;
 }
-float reluDerivative(float x) {
+inline float reluDerivative(float x) {
   return x > 0 ? 1.0f : 0.0f;
 }
-float sigmoid(float x) {
+inline float sigmoid(float x) {
   return 1.0f / (1.0f + expf(-x));
 }
-float sigmoidDerivative(float x) {
+inline float sigmoidDerivative(float x) {
   float s = sigmoid(x);
   return s * (1.0f - s);
 }
